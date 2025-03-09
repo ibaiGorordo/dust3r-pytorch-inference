d5161dc14607b70be998cce5251f358ffce1025d
Ibai Gorordo <43162939+ibaiGorordo@users.noreply.github.com>
2025-03-09 22:28:00 +0900

    Update example_image_pair.py

--- example_image_pair.py
+++ example_image_pair.py
@@ -7,32 +7,41 @@ from dust3r.postprocess import estimate_intrinsics, estimate_camera_pose
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-width, height = 512, 288
+width, height = 512, 352
 model_path = "models/DUSt3R_ViTLarge_BaseDecoder_512_dpt.pth"
 dust3r = Dust3r(model_path, width, height, device)
 
 # Read input images
-frame1 = cv2.imread("data/test1.jpg")
-frame2 = cv2.imread("data/test2.jpg")
-
-# Preprocess the input
-input1, frame1 = preprocess(frame1, width, height, device)
-input2, frame2 = preprocess(frame2, width, height, device)
+frame1 = cv2.imread("data/test4.jpg")
+frame2 = cv2.imread("data/test3.jpg")
 
 # Run Dust3r model
-pt1, cf1, pt2, cf2 = dust3r(input1, input2)
+pt1, cf1, pt2, cf2 = dust3r(frame1, frame2)
 
 # Postprocess the output
-pts1, colors1, conf_map1, depth_map1, mask1 = postprocess_with_color(pt1, cf1, frame1)
-pts2, colors2, conf_map2, depth_map2, mask2 = postprocess_with_color(pt2, cf2, frame2)
+threshold = 3.0
+pts1, colors1, conf_map1, depth_map1, mask1 = postprocess_with_color(pt1, cf1, frame1, threshold=threshold)
+pts2, colors2, conf_map2, depth_map2, mask2 = postprocess_with_color(pt2, cf2, frame2, threshold=threshold)
 
-# Estimate intrinsics and camera pose
+# Estimate intrinsics
 intrinsics1 = estimate_intrinsics(pts1, mask1)
 intrinsics2 = estimate_intrinsics(pts2, mask2)
-cam_pose1 = estimate_camera_pose(pts1, intrinsics1, mask1)
-cam_pose2 = estimate_camera_pose(pts2, intrinsics2, mask2)
+print(intrinsics2)
+
+# Estimate camera pose (the first one is the origin)
+cam_pose = estimate_camera_pose(pts2, intrinsics2, mask2)
+print(cam_pose)
 
 # Visualize the output
 rr.init("Dust3r Visualizer", spawn=True)
 rr.log("pts3d1", rr.Points3D(pts1, colors=colors1))
 rr.log("pts3d2", rr.Points3D(pts2, colors=colors2))
+
+rr.log("cam1/rgb", rr.Image(frame1))
+rr.log("cam1/depth", rr.DepthImage(depth_map1))
+rr.log("cam1", rr.Pinhole(image_from_camera=intrinsics1, width=width, height=height))
+
+rr.log("cam2/rgb", rr.Image(frame2))
+rr.log("cam2/depth", rr.DepthImage(depth_map2))
+rr.log("cam2", rr.Pinhole(image_from_camera=intrinsics2, width=width, height=height))
+rr.log("cam2", rr.Transform3D(mat3x3=cam_pose[:3, :3], translation=cam_pose[:3, 3]))
