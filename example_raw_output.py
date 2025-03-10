2176dd7c3a73780becb519ddd8a8086c5c5183e9
Ibai Gorordo <43162939+ibaiGorordo@users.noreply.github.com>
2025-03-10 22:44:32 +0900

    Update example_raw_output.py

--- example_raw_output.py
+++ example_raw_output.py
@@ -9,13 +9,13 @@ device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 conf_threshold = 3.0
 width, height = 512, 352
 model_path = "models/DUSt3R_ViTLarge_BaseDecoder_512_dpt.pth"
-dust3r = Dust3r(model_path, width, height, conf_threshold, device)
+dust3r = Dust3r(model_path, width, height, device=device, conf_threshold=conf_threshold)
 
 frame1 = cv2.imread("data/test1.jpg")
 frame2 = cv2.imread("data/test2.jpg")
 
-pts1, colors1, depth_map1, mask1, pts2, colors2, mask2 = dust3r(frame1, frame2)
+output1, output2 = dust3r(frame1, frame2)
 
 rr.init("Dust3r Visualizer", spawn=True)
-rr.log("pts3d1", rr.Points3D(pts1))
-rr.log("pts3d2", rr.Points3D(pts2))
+rr.log("pts3d1", rr.Points3D(output1.pts3d))
+rr.log("pts3d2", rr.Points3D(output2.pts3d))
